ae1d697e41f33aaeb775aa31fb2344f76794e379
Sartaj Singh <singhsartaj94@gmail.com>
2015-10-24 22:55:55 +0530

    create trackers symbolic link

--- configure.sh
+++ configure.sh
@@ -3,3 +3,4 @@ SCRIPT_DIR=$(pwd)
 ln -s "$SCRIPT_DIR/dotfiles/gitconfig" "$HOME/.gitconfig"
 ln -s "$SCRIPT_DIR/dotfiles/vimrc" "$HOME/.vimrc"
 ln -s "$SCRIPT_DIR/dotfiles/zshrc" "$HOME/.zshrc"
+ln -s "$SCRIPT_DIR/dotfiles/trackers" "$HOME/.trackers"
