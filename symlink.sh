29fb3d58574fe4a2fa01218be022f24ad7a0328b
Sartaj Singh <singhsartaj94@gmail.com>
2017-07-11 07:12:30 -0500

    mac compatible

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,5 @@
 SCRIPT_DIR=$(pwd)
 
 ln -s "$SCRIPT_DIR/dotfiles/gitconfig" "$HOME/.gitconfig"
-ln -s "$SCRIPT_DIR/dotfiles/vimrc" "$HOME/.vimrc"
 ln -s "$SCRIPT_DIR/dotfiles/zshrc" "$HOME/.zshrc"
-ln -s "$SCRIPT_DIR/dotfiles/trackers" "$HOME/.trackers"
+ln -s "$SCRIPT_DIR/dotfiles/vimrc" "$HOME/.vimrc"
